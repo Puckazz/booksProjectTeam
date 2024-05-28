8e2f761d572341c700a409bd0d184e61e832bb6c
NguyenNgocHai@gmail.com <haiproplayer@email.com>
2024-05-28 22:13:34 +0700

    change link page home of login

--- page_sanPham/dangNhap.php
+++ page_sanPham/dangNhap.php
@@ -79,7 +79,7 @@ $conn->close();
         </div>
         <div class="inner-menu">
           <ul>
-            <li><a href="../index.html">Trang Chủ</a></li>
+            <li><a href="../index.php">Trang Chủ</a></li>
             <li><a href="#">Nổi bật</a></li>
             <li><a href="#">Khuyến Mãi</a></li>
             <li><a href="./sanPhamGame.html">Sản Phẩm</a></li>
